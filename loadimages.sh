02a44397ed05f93e7ca1eea5c67b8fc721fa17e3
William Harrison <bizzk3t@gmail.com>
2014-12-11 20:15:08 -0600

    added the jpg file forrrrr no reason

--- loadimages.sh
+++ loadimages.sh
@@ -4,6 +4,10 @@ echo 'svg begin'
 dot -Tsvg chess.dot -o ./images/chess.svg
 echo 'svg complete'
 
+echo 'jpg begin'
+dot -Tjpg chess.dot -o ./images/chess.jpg
+echo 'jpg complete'
+
 echo 'png begin'
 dot -Tpng chess.dot -o ./images/chess.png
 echo 'png complete'
